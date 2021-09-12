53c0630833d981d2aa5e8403566f09fb5ed4b39f
Jake Seo <n00nietzsche@gmail.com>
2021-09-13 08:45:35 +0900

    Spring Boot를 이용한 RESTful Web Services 개발 #6 Spring Boot Project에서 Gradle 및 Profile 살펴보기

---
+++ src/main/java/com/example/inflearn_springboot_api/InflearnSpringbootApiApplication.java
@@ -0,0 +1,11 @@
+package com.example.inflearn_springboot_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflearnSpringbootApiApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(InflearnSpringbootApiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/inflearn_springboot_api/InflearnSpringbootApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inflearn_springboot_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflearnSpringbootApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
